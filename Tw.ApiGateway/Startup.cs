cae47b698295cc7d9d20f1be238f60378994b66f
littlewrong <28467353@qq.com>
2018-08-25 22:12:22 +0800

    添加项目文件。

---
+++ Tw.ApiGateway/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot().AddConsul();
+            services.AddMvcCore(option =>
+            {
+            }).AddAuthorization().AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5001";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "TWAPI";
+                });
+
+            services.AddMvc();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("ApiGateway", new Info { Title = "网关服务", Version = "v1" });
+                
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var apis = new List<string> { "BookingApi", "PassengerApi" };
+            app.UseMvc()
+               .UseSwagger()
+               .UseSwaggerUI(options =>
+               {
+                   apis.ForEach(m =>
+                   {
+                       options.SwaggerEndpoint($"/{m}/swagger.json", m);
+                   });
+               });
+
+            app.UseAuthentication();
+            
+            app.UseOcelot().Wait();
+        }
+    }
+}
